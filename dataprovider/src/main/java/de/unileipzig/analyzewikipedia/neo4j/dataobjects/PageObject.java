fba98d87c1d890834ad06b7e19c0a78b37dac46a
Pit Braunsdorf <pit.braunsdorf@comparex.de>
2017-09-11 21:48:45 +0200

    PageObject hinzugefügt

---
+++ dataprovider/src/main/java/de/unileipzig/analyzewikipedia/neo4j/dataobjects/PageObject.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.unileipzig.analyzewikipedia.neo4j.dataobjects;
+
+import de.unileipzig.analyzewikipedia.neo4j.constants.AnnotationKeys;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import org.neo4j.ogm.annotation.NodeEntity;
+import org.neo4j.ogm.annotation.Property;
+import org.neo4j.ogm.annotation.Relationship;
+
+/**
+ *
+ * @author Pit.Braunsdorf
+ */
+@NodeEntity(label="Page")
+public final class PageObject extends Entity{
+    
+    @Property(name = "title")
+    private String title;
+    
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    @Override
+    public String getTitle() {
+        return title;
+    }
+    
+    @Relationship(type = "contains", direction = Relationship.INCOMING)
+    List<Entity> entities;
+
+    public PageObject() {
+        this.title = "Active";
+    }
+    
+}
+
