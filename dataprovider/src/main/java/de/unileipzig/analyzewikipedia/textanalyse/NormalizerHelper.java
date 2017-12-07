b709abf4b5c339a78c1da9dd19b1281fb4c0951c
Danilo <Danilo@WORKZONE>
2017-12-07 18:31:40 +0100

    Small edits in NormalizerHelper.

--- dataprovider/src/main/java/de/unileipzig/analyzewikipedia/textanalyse/NormalizerHelper.java
+++ dataprovider/src/main/java/de/unileipzig/analyzewikipedia/textanalyse/NormalizerHelper.java
@@ -7,24 +7,43 @@ public class NormalizerHelper {
     
     public static String normalize(String lang, String sentences){
         
+        initLanguage(lang);
+        
+        String edit = editSentences(sentences);
+        
+        edit = standard(edit);
+                
+        return edit;
+        
+    }
+    
+    private static void initLanguage(String lang){
+        
         AnalyzerHelper.init(lang);
         SnowballStemmerHelper.init(lang);
         
-        String edit = sentences;
+    }
+    
+    private static String editSentences(String sentences){
         
-        if (AnalyzerHelper.isUsable()) {
-            edit = AnalyzerHelper.analyze(edit);
-        }
-        if (SnowballStemmerHelper.isUsable()) {
-            edit = SnowballStemmerHelper.analyze(edit);
+        if (AnalyzerHelper.isUsable()) sentences = AnalyzerHelper.analyze(sentences);
+        if (SnowballStemmerHelper.isUsable()) sentences = SnowballStemmerHelper.analyze(sentences);
+        
+        return sentences;
+        
+    }
+    
+    private static String standard(String sentences){
+        
+        if (
+                !AnalyzerHelper.isUsable() &&
+                !SnowballStemmerHelper.isUsable()
+            )
+        {
+            sentences = AnalyzerHelper.standard(sentences);
         }
         
-        if (    !AnalyzerHelper.isUsable() &&
-                !SnowballStemmerHelper.isUsable()    ) {
-            edit = AnalyzerHelper.standard(sentences);
-        }
-                
-        return edit;
+        return sentences;
         
     }
        
